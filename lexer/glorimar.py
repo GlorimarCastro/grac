842d539cdc5fb372950fe4c8cacfeaf41df56d7a
Glorimar Castro <glorimar.castro@upr.edu>
2015-11-03 23:56:26 -0400

    more ply rules

--- lexer/glorimar.py
+++ lexer/glorimar.py
@@ -1,43 +1,47 @@
-'''
-Created on Nov 3, 2015
-
-@author: galarwen
-'''
 import ply.lex as lex
 from ply.lex import TOKEN
 import re
-import itertools as iter
 
 #reserved words:
 reservedWords = {
-    'CLASSIFIER': ['svc\(\)','dtc\(\)', 'nnc\(\)'],
+    'CLASSIFIER'  : ['svc\(\)','dtc\(\)', 'nnc\(\)'],
     'CLASSIFIER_METHOD': ['getErrorRate\(\)', 'saveErrorRate\(\)', 'predict', 'execute\(\)'],
     'COMMENT': ['#'],
     'INT':'int',
-    'CROSSVALIDATION_VAR':['doCrossValidation', 'k_foldd']
+    'CROSSVALIDATION_VAR':['doCrossValidation', 'k_foldd'],
+    'UPLOAD_COMMAND':['uploadTrainingData', 'uploadTestData', 'uploadData'],
+    'CSV_VAR':['hasHeader','classColumn','featuresColumns','saveResult'],
+    'BOOLEAN':['true', 'false'],
+    'ALL': 'all'
+
 }
 
 #list of token names
 tokens = [
-          
+       'PATH'   
 ] + list(reservedWords)#+ list(iter.chain.from_iterable(reservedWords.values())) 
 
-literals = ['(',')','{','}']
+literals = ['(',')','{','}', '=']
 #------------------------------------------------------
 #        REG DECLARATIONS - @TOKEN DECORATORS
 #------------------------------------------------------
 reg_classifiers = re.compile('|'.join(reservedWords['CLASSIFIER']))
 reg_classifiers_methods = re.compile('|'.join(reservedWords['CLASSIFIER_METHOD']))
 reg_crossvalidation = re.compile('|'.join(reservedWords['CROSSVALIDATION_VAR']))
-
-
+reg_uploadcommand = re.compile('|'.join(reservedWords['UPLOAD_COMMAND']))
+reg_csvvar = re.compile('|'.join(reservedWords['CSV_VAR']))
+reg_boolean = re.compile('|'.join(reservedWords['BOOLEAN']))
 #------------------------------------------------------
 #        SIMPLE TOKEN DEFINITION
 #----------------------------------------------------
 t_ignore = ' \t'
+t_ALL = r'all'
 #------------------------------------------------------
 #        TOKEN DEFINITION WITH FUNCTION
 #------------------------------------------------------
+def t_PATH(t):
+    r'\"(.+?)\"'
+    return t
 #Rule for classifiers:
 @TOKEN(reg_classifiers.pattern)
 def t_CLASSIFIER(t):
@@ -52,6 +56,19 @@ def t_CLASSIFIER_METHOD(t):
 @TOKEN(reg_crossvalidation.pattern)
 def t_CROSSVALIDATION_VAR(t):
     return t
+
+@TOKEN(reg_csvvar.pattern)
+def t_CSV_VAR(t):
+    return t
+
+@TOKEN(reg_boolean.pattern)
+def t_BOOLEAN(t):
+    return t
+#rule for upload
+@TOKEN(reg_uploadcommand.pattern)
+def t_UPLOAD_COMMAND(t):
+    return t
+
 # Define a rule so we can track line numbers 
 def t_newline(t):     
     r'\n+'     
@@ -84,11 +101,14 @@ def find_column(input,token):
     column = (token.lexpos - last_cr) + 1
     return column
 
-
+#=========================================================================
 #test
+#=========================================================================
 data2test = """
-svc()dtc()DTC()SvC() doCrossValidation execute() #hola 
-svc()965
+svc()dtc()DTC()"SvC()" doCrossValidation execute() #hola 
+svc()965() all true
+uploadData
+hasHeader
 """
 lexer = lex.lex(reflags=re.UNICODE|re.IGNORECASE)
 lexer.input(data2test)
